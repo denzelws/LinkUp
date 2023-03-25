20eaf54b45b16d10e9ea787f58a816038a40b2b6
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-03-25 01:46:16 -0300

    Add TextField initial tests

--- client/src/components/TextField/test.tsx
+++ client/src/components/TextField/test.tsx
@@ -3,13 +3,22 @@ import React from 'react'
 
 global.React = React
 
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import TextField from '.'
 import { renderWithTheme } from '../../utils/tests/helpers'
+import { Search } from '@styled-icons/material-outlined'
 
 describe('<TextField />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<TextField />)
+  it('should render with placeholder ', () => {
+    renderWithTheme(<TextField placeholder="text" />)
+
+    expect(screen.getByPlaceholderText(/text/i)).toBeInTheDocument()
+  })
+
+  it('should render with icon', () => {
+    renderWithTheme(<TextField icon={<Search data-testid="icon" />} />)
+
+    expect(screen.getByTestId(/icon/i)).toBeInTheDocument()
   })
 })
