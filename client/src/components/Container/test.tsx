0fb0ff1e788419cd261f1f8e0a3a67af285859f7
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-03-25 01:34:04 -0300

    Add Container tests and inline snapshot

--- client/src/components/Container/test.tsx
+++ client/src/components/Container/test.tsx
@@ -3,14 +3,38 @@ import React from 'react'
 
 global.React = React
 
-import { screen } from '@testing-library/react'
+import theme from '../../styles/theme'
 
 import { renderWithTheme } from '../../utils/tests/helpers'
-
 import { Container } from '.'
 
 describe('<Container />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<Container />)
+    const { container } = renderWithTheme(
+      <Container>
+        <span>NoteTest</span>
+      </Container>
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container
+    )
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        width: 100%;
+        max-width: 50rem;
+        margin: 0 auto;
+      }
+
+      <div
+        class="c0"
+      >
+        <span>
+          NoteTest
+        </span>
+      </div>
+    `)
   })
 })
