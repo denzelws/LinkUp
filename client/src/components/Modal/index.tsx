ad7c690d83cb86fd20b6e9893fe9580b491c176f
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-03-30 11:46:37 -0300

    Add use of React Portals to Modal

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,23 +1,29 @@
 import Button from '../Button'
 import * as S from './styles'
 
+import ReactDOM from 'react-dom'
+
 export type ModalProps = {
   danger?: boolean
 }
 
-const Modal = ({ danger }: ModalProps) => (
-  <S.OverlayBox>
-    <S.ModalContainer>
-      <S.Title danger={danger}>Título do modal</S.Title>
-      <S.WarningText>Esta ação não poderá ser desfeita!</S.WarningText>
-      <S.ButtonBox>
-        <S.CancelButton>Cancelar</S.CancelButton>
-        <Button danger={danger} size="small">
-          Deletar
-        </Button>
-      </S.ButtonBox>
-    </S.ModalContainer>
-  </S.OverlayBox>
-)
+const modalRoot = document.getElementById('modal-root') as Element
+
+const Modal = ({ danger }: ModalProps) =>
+  ReactDOM.createPortal(
+    <S.OverlayBox>
+      <S.ModalContainer>
+        <S.Title danger={danger}>Título do modal</S.Title>
+        <S.WarningText>Esta ação não poderá ser desfeita!</S.WarningText>
+        <S.ButtonBox>
+          <S.CancelButton>Cancelar</S.CancelButton>
+          <Button danger={danger} size="small">
+            Deletar
+          </Button>
+        </S.ButtonBox>
+      </S.ModalContainer>
+    </S.OverlayBox>,
+    modalRoot
+  )
 
 export default Modal
