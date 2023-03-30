8fb928e46da600884425cf7a35246eed475ec217
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-03-30 17:42:50 -0300

    Add handleSubmit method and states to ContactForm

--- client/src/components/ContactForm/index.tsx
+++ client/src/components/ContactForm/index.tsx
@@ -2,36 +2,68 @@ import FormGroup from '../FormGroup'
 import { Input, Select } from '../../templates/NewContact/styles'
 import * as S from './styles'
 import Button from '../Button'
+import { FormEvent, useState } from 'react'
 
 export type ContactFormProps = {
   buttonLabel: string
   error?: boolean
 }
 
-const ContactForm = ({ buttonLabel }: ContactFormProps) => (
-  <S.WrapperForm>
-    <FormGroup>
-      <Input placeholder="Nome" />
-    </FormGroup>
+const ContactForm = ({ buttonLabel }: ContactFormProps) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [category, setCategory] = useState('')
 
-    <FormGroup error="O formato do e-mail é inválido">
-      <Input placeholder="Email" error />
-    </FormGroup>
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log({
+      name,
+      email,
+      phone,
+      category
+    })
+  }
 
-    <FormGroup>
-      <Input placeholder="Senha" />
-    </FormGroup>
+  return (
+    <S.WrapperForm onSubmit={handleSubmit}>
+      <FormGroup>
+        <Input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Nome"
+        />
+      </FormGroup>
 
-    <FormGroup>
-      <Select>
-        <option value="instagram">Instagram</option>
-      </Select>
-    </FormGroup>
+      <FormGroup>
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+      </FormGroup>
 
-    <S.ButtonContainer>
-      <Button size="fullWidth">{buttonLabel}</Button>
-    </S.ButtonContainer>
-  </S.WrapperForm>
-)
+      <FormGroup>
+        <Input
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="Telefone"
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="category">Categoria</option>
+          <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
+        </Select>
+      </FormGroup>
+
+      <S.ButtonContainer>
+        <Button size="fullWidth">{buttonLabel}</Button>
+      </S.ButtonContainer>
+    </S.WrapperForm>
+  )
+}
 
 export default ContactForm
