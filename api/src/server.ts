98a79b4deb7ded41227cfc555a31f2f965cc42bb
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-03 09:55:49 -0300

    Add corsMiddleware to server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { client } from './database'
 import routes from './routes'
+import corsMiddleware from './app/middlewares/cors'
 
 const app = express()
 
 app.use(express.json())
-
+app.use(corsMiddleware)
 app.use(routes)
 
 app.listen(3333, () => {
