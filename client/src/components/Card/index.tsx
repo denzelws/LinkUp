78b77cf6f0645b8753f302b70b9c00e7f38faa75
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-06-06 15:28:24 -0300

    Add id to CardProps

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -5,25 +5,26 @@ import { Link } from 'react-router-dom'
 import * as S from './styles'
 
 export type CardProps = {
+  id: string
   name: string
   platform: string
   email: string
   phone: string
 }
 
-const Card = ({ email, platform, phone, name }: CardProps) => (
+const Card = ({ email, platform, phone, name, id }: CardProps) => (
   <S.Wrapper>
     <S.InfoBox>
       <S.ContactName>
         <strong>{name}</strong>
-        <small>{platform}</small>
+        {platform && <small>{platform}</small>}
       </S.ContactName>
       <span>{email}</span>
       <span>{phone}</span>
     </S.InfoBox>
 
     <S.Actions>
-      <Link to="/edit/:id">
+      <Link to={`/edit/${id}`}>
         <S.EditButton>
           <Edit aria-label="Edit contact" />
         </S.EditButton>
