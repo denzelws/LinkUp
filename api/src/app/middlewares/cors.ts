fd6cfe3870376c70b8ce6ca0bd144700b63910f3
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-06-06 15:27:19 -0300

    Update cors middleware

--- api/src/app/middlewares/cors.ts
+++ api/src/app/middlewares/cors.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from 'express'
 
 const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Max-Age', '10')
   next()
 }
 
