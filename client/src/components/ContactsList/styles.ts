d2ba9fe0b326f3af84f1398742c0a76a392f5f9d
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-06-19 21:26:39 -0300

    Add DetailsProps types at styles

--- client/src/components/ContactsList/styles.ts
+++ client/src/components/ContactsList/styles.ts
@@ -1,17 +1,28 @@
 import styled, { css } from 'styled-components'
 import { ArrowLeftShort } from '@styled-icons/bootstrap/ArrowLeftShort'
 
+type DetailsProps = {
+  hasError: boolean
+  contactsLength: number
+}
+
 export const Wrapper = styled.main`
   ${({ theme }) => css`
     margin-top: ${theme.spacings.medium};
   `}
 `
 
-export const Details = styled.div`
-  ${({ theme }) => css`
+export const Details = styled.div<DetailsProps>`
+  ${({ theme, hasError, contactsLength }) => css`
     display: flex;
-    justify-content: space-between;
+    justify-content: ${hasError
+      ? 'flex-end'
+      : contactsLength > 0
+      ? 'space-between'
+      : 'center'};
     margin-bottom: ${theme.spacings.small};
+    border-bottom: 0.2rem solid #e5e5e5;
+    padding-bottom: ${theme.spacings.xsmall};
   `}
 `
 
@@ -46,6 +57,28 @@ export const Button = styled.button`
 export const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  img {
+    width: 10rem;
+  }
+`
+
+export const EmptyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`
+
+export const EmptyText = styled.p`
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.xxsmall};
+    color: ${theme.colors.gray};
+
+    strong {
+      color: ${theme.colors.primaryMain};
+    }
+  `}
 `
 
 export const ListHeader = styled.header`
@@ -76,16 +109,39 @@ export const Card = styled.div`
 `
 
 type IconProps = {
-  orderBy: string
+  orderby: string
 }
 
 export const ArrowIcon = styled(ArrowLeftShort)<IconProps>`
-  ${({ theme, orderBy }) => css`
+  ${({ theme, orderby }) => css`
     margin-right: 0.2rem;
     justify-content: center;
     width: 3rem;
     color: ${theme.colors.blue};
-    transform: ${orderBy === 'asc' ? 'rotate(90deg)' : 'rotate(270deg)'};
+    transform: ${orderby === 'asc' ? 'rotate(90deg)' : 'rotate(270deg)'};
     transition: transform 0.2s ease-in;
+  `}
+`
+
+export const ErrorContainer = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 10rem;
+  }
+`
+
+export const InfoBox = styled.div`
+  ${({ theme }) => css`
+    margin-left: ${theme.spacings.small};
+  `}
+`
+
+export const WarningText = styled.strong`
+  ${({ theme }) => css`
+    display: block;
+    font-size: ${theme.font.sizes.xlarge};
+    color: ${theme.colors.danger.main};
+    margin-bottom: ${theme.spacings.xxsmall};
   `}
 `
