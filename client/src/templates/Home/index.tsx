71521da0dfe29c37eec0ee0c9834cb2c81d57255
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-06-19 21:28:27 -0300

    Implement contactCount and handleContactCountChange

--- client/src/templates/Home/index.tsx
+++ client/src/templates/Home/index.tsx
@@ -5,18 +5,27 @@ import Header from '../../components/Header'
 
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState('')
+  const [contactCount, setContactCount] = useState(0)
 
   const handleChangeSearchTerm = (newSearchTerm: string) => {
     setSearchTerm(newSearchTerm)
   }
 
+  const handleContactCountChange = (count: number) => {
+    setContactCount(count)
+  }
+
   return (
     <Container>
       <Header
+        withoutInput={contactCount === 0}
         searchTerm={searchTerm}
         onChangeSearchTerm={handleChangeSearchTerm}
       />
-      <ContactsList searchTerm={searchTerm} />
+      <ContactsList
+        searchTerm={searchTerm}
+        onContactCountChange={handleContactCountChange}
+      />
     </Container>
   )
 }
