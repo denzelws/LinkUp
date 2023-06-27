3094727853212fe9d3b477db9ad18dbc39588131
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-06-27 11:08:22 -0300

    Create function isValidUUID

---
+++ api/src/app/utils/isValidUUID.ts
@@ -0,0 +1,5 @@
+export default function isValidUUID(id: string): boolean {
+  const regex =
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
+  return regex.test(id)
+}
