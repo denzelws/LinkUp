93f45d74ed0f5677cd2356b369c302d7e5fad962
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-06-27 11:46:28 -0300

    Add callback function handleSubmit

--- client/src/templates/NewContact/index.tsx
+++ client/src/templates/NewContact/index.tsx
@@ -1,12 +1,27 @@
 import { Container } from '../../components/Container'
 import PageHeader from '../../components/PageHeader'
 import ContactForm from '../../components/ContactForm'
+import { CardProps } from '../../components/Card'
+import contactsService from '../../services/ContactsService'
 
-const NewContact = () => (
-  <Container>
-    <PageHeader title="Novo Contato" />
-    <ContactForm buttonLabel="Cadastrar" />
-  </Container>
-)
+const NewContact = () => {
+  const handleSubmit = async (formData: CardProps) => {
+    const contact = {
+      name: formData.name,
+      email: formData.email,
+      phone: formData.phone,
+      category_id: formData.categoryId
+    }
+
+    const response = await contactsService.createContact(contact)
+    console.log(response)
+  }
+  return (
+    <Container>
+      <PageHeader title="Novo Contato" />
+      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
+    </Container>
+  )
+}
 
 export default NewContact
