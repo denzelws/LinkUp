f495a1cba0ae13baa291e7609fd6dde4342f39fb
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-06-30 15:28:24 -0300

    Adjust App.tsx

--- client/src/pages/App/App.tsx
+++ client/src/pages/App/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import EditContact from '../../templates/EditContact'
 import Home from '../../templates/Home'
@@ -6,13 +6,11 @@ import NewContact from '../../templates/NewContact'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<NewContact />} />
-        <Route path="/edit/:id" element={<EditContact />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<NewContact />} />
+      <Route path="/edit/:id" element={<EditContact />} />
+    </Routes>
   )
 }
 
