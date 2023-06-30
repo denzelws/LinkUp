ac3ea8eed700b476154da47fd8fdb5ce3aa87561
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-06-30 15:28:02 -0300

    Adjust main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,13 +6,16 @@ import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 import App from './pages/App/App'
 import ToastContainer from './components/Toast/ToastContainer'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <ToastContainer />
-      <App />
+      <BrowserRouter>
+        <ToastContainer />
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 )
