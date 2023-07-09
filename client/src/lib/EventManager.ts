7e43dc5e856982441e50dbcaffce01e980256dcf
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-09 14:46:25 -0300

    Create EventManager lib class

---
+++ client/src/lib/EventManager.ts
@@ -0,0 +1,46 @@
+type EventListenerProps = (payload: PayloadProps) => void
+
+type PayloadProps = {
+  text: string
+  type: string
+  duration?: number
+}
+// { [event: string]: EventListenerProps[] }
+export class EventManager {
+  private listeners: Map<any, any>
+
+  constructor() {
+    this.listeners = new Map()
+  }
+
+  public on(event: string, listener: EventListenerProps): void {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, [])
+    }
+
+    this.listeners.get(event).push(listener)
+  }
+
+  public emit(event: string, payload: PayloadProps): void {
+    if (!this.listeners.has(event)) {
+      return
+    }
+
+    this.listeners.get(event).forEach((listener) => {
+      listener(payload)
+    })
+  }
+
+  removeListener(event: string, listenerToRemove) {
+    const listeners = this.listeners.get(event)
+    if (!listeners) {
+      return
+    }
+
+    const filteredListeners = listeners.filter(
+      (listener) => listener !== listenerToRemove
+    )
+
+    this.listeners.set(event, filteredListeners)
+  }
+}
