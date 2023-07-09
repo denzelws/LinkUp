0cead4dce5da0daebc417fc401111c3f0287d65a
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-09 14:36:18 -0300

    Update ContactForm add useImperativeHandle to handle fields value

--- client/src/components/ContactForm/index.tsx
+++ client/src/components/ContactForm/index.tsx
@@ -1,18 +1,27 @@
-import { FormEvent, useEffect, useState } from 'react'
+/* eslint-disable react/display-name */
+import {
+  FormEvent,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useState
+} from 'react'
 
 import { useError } from '../../hooks/useError'
-import { Input, Select } from '../../templates/NewContact/styles'
 
+import { CardProps } from '../Card'
+import { Input, Select } from '../../templates/NewContact/styles'
 import FormGroup from '../FormGroup'
 import Button from '../Button'
-import isEmailValid from '../../utils/isEmailValid'
 
-import * as S from './styles'
+import isEmailValid from '../../utils/isEmailValid'
 import formatPhone from '../../utils/formatPhone'
+
 import categoriesService, {
   CategoriesProps
 } from '../../services/CategoriesService'
-import { CardProps } from '../Card'
+
+import * as S from './styles'
 
 export type ContactFormProps = {
   buttonLabel: string
@@ -20,129 +29,151 @@ export type ContactFormProps = {
   onSubmit: (formData: CardProps) => Promise<void>
 }
 
-const ContactForm = ({ buttonLabel, onSubmit }: ContactFormProps) => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [categoryId, setCategoryId] = useState('')
-  const [categories, setCategories] = useState<CategoriesProps[]>([])
-  const [isLoadingCategories, setIsLoadingCategories] = useState(true)
-  const [isSubmitting, setIsSubmitting] = useState(false)
+const ContactForm = forwardRef(
+  ({ buttonLabel, onSubmit }: ContactFormProps, ref) => {
+    console.log('ContactForm.ref', ref)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [categoryId, setCategoryId] = useState('')
+    const [categories, setCategories] = useState<CategoriesProps[]>([])
+    const [isLoadingCategories, setIsLoadingCategories] = useState(true)
+    const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const { errors, setError, removeError, getErrorMessageByFieldName } =
-    useError()
+    const { errors, setError, removeError, getErrorMessageByFieldName } =
+      useError()
 
-  const isFormValid = name && errors.length === 0
+    const isFormValid = name && errors.length === 0
 
-  useEffect(() => {
-    async function loadCategories() {
-      try {
-        const categoriesList = await categoriesService.listCategories()
-        setCategories(categoriesList)
-      } catch {
-      } finally {
-        setIsLoadingCategories(false)
+    useImperativeHandle(
+      ref,
+      () => ({
+        SetFieldsValue: (contact: CardProps) => {
+          setName(contact.name ?? '')
+          setEmail(contact.email ?? '')
+          setPhone(formatPhone(contact.phone ?? ''))
+          setCategoryId(contact.categoryId ?? '')
+        },
+        resetFields: () => {
+          setName('')
+          setEmail('')
+          setPhone('')
+          setCategoryId('')
+        }
+      }),
+      []
+    )
+
+    useEffect(() => {
+      async function loadCategories() {
+        try {
+          const categoriesList = await categoriesService.listCategories()
+          setCategories(categoriesList)
+        } catch {
+        } finally {
+          setIsLoadingCategories(false)
+        }
+      }
+
+      loadCategories()
+    }, [])
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+
+      setIsSubmitting(true)
+
+      await onSubmit({
+        name,
+        email,
+        phone,
+        categoryId
+      })
+
+      setIsSubmitting(false)
+    }
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setName(e.target.value)
+
+      if (!e.target.value) {
+        setError({ field: 'name', message: 'Nome é obrigatório' })
+      } else {
+        removeError('name')
       }
     }
 
-    loadCategories()
-  }, [])
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(e.target.value)
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    setIsSubmitting(true)
-
-    await onSubmit({
-      name,
-      email,
-      phone,
-      categoryId
-    })
-
-    setIsSubmitting(false)
-  }
-
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value)
-
-    if (!e.target.value) {
-      setError({ field: 'name', message: 'Nome é obrigatório' })
-    } else {
-      removeError('name')
+      if (e.target.value && !isEmailValid(e.target.value)) {
+        setError({ field: 'email', message: 'E-mail inválido' })
+      } else {
+        removeError('email')
+      }
     }
-  }
 
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value)
-
-    if (e.target.value && !isEmailValid(e.target.value)) {
-      setError({ field: 'email', message: 'E-mail inválido' })
-    } else {
-      removeError('email')
+    const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPhone(formatPhone(e.target.value))
     }
+
+    return (
+      <S.WrapperForm onSubmit={handleSubmit} noValidate>
+        <FormGroup error={getErrorMessageByFieldName('name')}>
+          <Input
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Nome"
+            error={!!getErrorMessageByFieldName('name')}
+          />
+        </FormGroup>
+
+        <FormGroup error={getErrorMessageByFieldName('email')}>
+          <Input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Email"
+            error={!!getErrorMessageByFieldName('email')}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Input
+            value={phone}
+            onChange={handlePhoneChange}
+            placeholder="Telefone"
+            maxLength={15}
+          />
+        </FormGroup>
+
+        <FormGroup isLoading={isLoadingCategories}>
+          <Select
+            value={categoryId}
+            onChange={(e) => setCategoryId(e.target.value)}
+            disabled={isLoadingCategories}
+          >
+            <option value="">Sem categoria</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </Select>
+        </FormGroup>
+
+        <S.ButtonContainer>
+          <Button
+            size="fullWidth"
+            disabled={!isFormValid}
+            isLoading={isSubmitting}
+          >
+            {buttonLabel}
+          </Button>
+        </S.ButtonContainer>
+      </S.WrapperForm>
+    )
   }
-
-  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPhone(formatPhone(e.target.value))
-  }
-
-  return (
-    <S.WrapperForm onSubmit={handleSubmit} noValidate>
-      <FormGroup error={getErrorMessageByFieldName('name')}>
-        <Input
-          value={name}
-          onChange={handleNameChange}
-          placeholder="Nome"
-          error={!!getErrorMessageByFieldName('name')}
-        />
-      </FormGroup>
-
-      <FormGroup error={getErrorMessageByFieldName('email')}>
-        <Input
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-          placeholder="Email"
-          error={!!getErrorMessageByFieldName('email')}
-        />
-      </FormGroup>
-
-      <FormGroup>
-        <Input
-          value={phone}
-          onChange={handlePhoneChange}
-          placeholder="Telefone"
-          maxLength={15}
-        />
-      </FormGroup>
-
-      <FormGroup isLoading={isLoadingCategories}>
-        <Select
-          value={categoryId}
-          onChange={(e) => setCategoryId(e.target.value)}
-          disabled={isLoadingCategories}
-        >
-          <option value="">Sem categoria</option>
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </Select>
-      </FormGroup>
-
-      <S.ButtonContainer>
-        <Button
-          size="fullWidth"
-          disabled={!isFormValid}
-          isLoading={isSubmitting}
-        >
-          {buttonLabel}
-        </Button>
-      </S.ButtonContainer>
-    </S.WrapperForm>
-  )
-}
+)
 
 export default ContactForm
