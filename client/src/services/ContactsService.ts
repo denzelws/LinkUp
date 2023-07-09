b0dbbd1d82db41feaf238ad0b9130ea45a626fb7
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-09 14:47:06 -0300

    Update at ContactsService add ContactMapper

--- client/src/services/ContactsService.ts
+++ client/src/services/ContactsService.ts
@@ -1,17 +1,35 @@
 import { CardProps } from '../components/Card'
+import ContactMapper from './mappers/ContactMapper'
 import createHttpClient from './utils/HttpClient'
 
 const contactsService = {
   httpClient: createHttpClient('http://localhost:3333'),
 
   async listContacts(orderBy: string): Promise<CardProps[]> {
-    return this.httpClient.get(`/contact/?orderBy=${orderBy}`)
+    const contacts = await this.httpClient.get(`/contact/?orderBy=${orderBy}`)
+    return (contacts as CardProps[]).map(ContactMapper.toDomain)
   },
 
-  async createContact(contact: CardProps) {
+  async getContactById(id: string): Promise<CardProps> {
+    const contact = await this.httpClient.get(`/contact/${id}`)
+
+    return ContactMapper.toDomain(contact as CardProps)
+  },
+
+  createContact(contact: CardProps) {
+    const body = ContactMapper.toPersistance(contact)
     return this.httpClient.post('/contact', {
-      body: contact
+      body
     })
+  },
+
+  updateContact(id: string | undefined, contact: CardProps) {
+    const body = ContactMapper.toPersistance(contact)
+    return this.httpClient.put(`/contact/${id}`, { body })
+  },
+
+  deleteContact(id: string | undefined) {
+    return this.httpClient.deleteContact(`/contact/${id}`)
   }
 }
 
