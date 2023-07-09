8ea92e9f9a72f4e5663070128b175978a0ac1f58
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-09 14:35:26 -0300

    Update CardSlider component with onDeleteContact prop

--- client/src/components/CardSlider/index.tsx
+++ client/src/components/CardSlider/index.tsx
@@ -4,24 +4,29 @@ import * as S from './styles'
 
 export type CardSliderProps = {
   contacts: CardProps[]
+  onDeleteContact: (contactId: string) => void
 }
 
 const settings: SliderSettings = {
   arrows: false,
   vertical: true,
   verticalSwiping: true,
-  infinite: false,
-  slidesToShow: 3
+  infinite: true,
+  touchMove: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
 }
 
-const CardSlider = ({ contacts }: CardSliderProps) => (
-  <S.Wrapper>
-    <Slider settings={settings}>
-      {contacts.map((contact) => (
-        <Card key={contact.id} {...contact} />
-      ))}
-    </Slider>
-  </S.Wrapper>
-)
+const CardSlider = ({ contacts, onDeleteContact }: CardSliderProps) => {
+  return (
+    <S.Wrapper>
+      <Slider settings={settings}>
+        {contacts.map((contact) => (
+          <Card key={contact.id} {...contact} onDelete={onDeleteContact} />
+        ))}
+      </Slider>
+    </S.Wrapper>
+  )
+}
 
 export default CardSlider
