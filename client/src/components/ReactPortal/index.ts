caec86c0131c32341013fa22115a47e41f6ee6ab
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-09 14:38:56 -0300

    Create ReactPortal component

---
+++ client/src/components/ReactPortal/index.ts
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom'
+
+type ReactPortalProps = {
+  containerId?: string
+  children: React.ReactNode
+}
+
+export default function ReactPortal({
+  containerId = 'portal-root',
+  children
+}: ReactPortalProps) {
+  let container = document.getElementById(containerId)
+
+  if (!container) {
+    container = document.createElement('div')
+    container.setAttribute('id', containerId)
+    document.body.appendChild(container)
+  }
+
+  return ReactDOM.createPortal(children, container)
+}
