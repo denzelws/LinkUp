ab816624a40ab3b44b1446c829eea5aa1dfe92c6
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-09 14:34:18 -0300

    Add prop onDelete, category_id and states at Card

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,40 +1,104 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 import { Edit } from '@styled-icons/boxicons-solid'
 import { Trash } from '@styled-icons/octicons'
-import { Link } from 'react-router-dom'
+
+import Modal from '../Modal'
+import contactsService from '../../services/ContactsService'
+import { toast } from '../../utils/toast'
 
 import * as S from './styles'
 
 export type CardProps = {
-  id?: string
+  id?: string | undefined
   name: string
   category_name?: string
   categoryId?: string
+  category_id?: string
   email: string
   phone: string
+  onDelete?: (contactId: string) => void
 }
 
-const Card = ({ email, category_name, phone, name, id }: CardProps) => (
-  <S.Wrapper>
-    <S.InfoBox>
-      <S.ContactName>
-        <strong>{name}</strong>
-        {category_name && <small>{category_name}</small>}
-      </S.ContactName>
-      <span>{email}</span>
-      <span>{phone}</span>
-    </S.InfoBox>
+const Card = ({
+  email,
+  category_name,
+  phone,
+  name,
+  id,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onDelete = () => {}
+}: CardProps) => {
+  const [isDeleteModalVisible, setIsModalVisible] = useState(false)
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false)
 
-    <S.Actions>
-      <Link to={`/edit/${id}`}>
-        <S.EditButton>
-          <Edit aria-label="Edit contact" />
-        </S.EditButton>
-      </Link>
-      <S.DeleteButton>
-        <Trash aria-label="Delete contact" />
-      </S.DeleteButton>
-    </S.Actions>
-  </S.Wrapper>
-)
+  const handleDeleteModalVisible = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleRemoveModal = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleConfirmDeleteContact = async () => {
+    try {
+      setIsLoadingDelete(true)
+      await contactsService.deleteContact(id)
+      toast({
+        type: 'success',
+        text: 'Contato deletado com sucesso!'
+      })
+      onDelete(id ?? '')
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao deletar o contato!'
+      })
+    } finally {
+      setIsLoadingDelete(false)
+      handleRemoveModal()
+    }
+    console.log(id)
+  }
+
+  return (
+    <S.Wrapper>
+      <Modal
+        danger
+        isLoading={isLoadingDelete}
+        visible={isDeleteModalVisible}
+        title={`Tem certeza que deseja remover o contato "${name}"?`}
+        confirmLabel="Deletar"
+        onCancel={handleRemoveModal}
+        onConfirm={handleConfirmDeleteContact}
+      >
+        Esta ação não poderá ser desfeita!
+      </Modal>
+      <S.InfoBox>
+        <S.ContactName>
+          <strong>{name}</strong>
+          {category_name && <small>{category_name}</small>}
+        </S.ContactName>
+        <span>{email}</span>
+        <span>{phone}</span>
+      </S.InfoBox>
+
+      <S.Actions>
+        <Link to={`/edit/${id}`}>
+          <S.EditButton>
+            <Edit aria-label="Edit contact" />
+          </S.EditButton>
+        </Link>
+        <S.DeleteButton>
+          <Trash
+            aria-label="Delete contact"
+            onClick={handleDeleteModalVisible}
+          />
+        </S.DeleteButton>
+      </S.Actions>
+    </S.Wrapper>
+  )
+}
 
 export default Card
