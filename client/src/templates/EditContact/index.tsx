c3c9f6741d1384d060cc30ac6d4000adee40ea3a
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-09 14:49:02 -0300

    Add toast to EditContact to handle event messages

--- client/src/templates/EditContact/index.tsx
+++ client/src/templates/EditContact/index.tsx
@@ -1,12 +1,84 @@
+import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect, useRef, useState } from 'react'
+
 import { Container } from '../../components/Container'
+import { CardProps } from '../../components/Card'
 import PageHeader from '../../components/PageHeader'
 import ContactForm from '../../components/ContactForm'
+import Loader from '../../components/Loader'
 
-const EditContact = () => (
-  <Container>
-    <PageHeader title="Editar Contato" />
-    <ContactForm buttonLabel="Salvar alterações" />
-  </Container>
-)
+import contactsService from '../../services/ContactsService'
+import { toast } from '../../utils/toast'
+
+import { useSafeAsyncAction } from '../../hooks/useSafeAsyncAction'
+
+const EditContact = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [contactName, setContactName] = useState('')
+
+  const contactFormRef = useRef<HTMLFormElement>(null)
+
+  const { id } = useParams<{ id: string }>()
+  const navigate = useNavigate()
+
+  const safeAsyncAction = useSafeAsyncAction()
+
+  useEffect(() => {
+    async function loadContacts() {
+      try {
+        const contact: CardProps = await contactsService.getContactById(
+          id ?? ''
+        )
+
+        safeAsyncAction(() => {
+          contactFormRef.current?.SetFieldsValue(contact)
+          setIsLoading(false)
+          setContactName(contact.name)
+        })
+      } catch {
+        safeAsyncAction(() => {
+          navigate('/')
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado!'
+          })
+        })
+      }
+    }
+
+    loadContacts()
+  }, [id, navigate, safeAsyncAction])
+
+  async function handleSubmit(contact: CardProps): Promise<void> {
+    try {
+      await contactsService.updateContact(id, contact)
+      setContactName(contact.name)
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!'
+      })
+    } catch {
+      navigate('/')
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!'
+      })
+    }
+  }
+  return (
+    <Container>
+      <Loader isLoading={isLoading} />
+      <PageHeader
+        title={isLoading ? 'Carregando...' : `Editar ${contactName}`}
+      />
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
+    </Container>
+  )
+}
 
 export default EditContact
