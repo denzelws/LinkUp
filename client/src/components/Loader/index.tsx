e5b1370f812c4c368dc11ae3acc6d01e2b809388
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-13 18:02:09 -0300

    Implement useAnimatedUnmount to Loader

--- client/src/components/Loader/index.tsx
+++ client/src/components/Loader/index.tsx
@@ -2,19 +2,22 @@ import { Spinner } from '../Spinner'
 import ReactPortal from '../ReactPortal'
 
 import * as S from './styles'
+import { useAnimatedUnmount } from '../../hooks/useAnimatedUnmount'
 
 type LoaderProps = {
   isLoading: boolean
 }
 
 const Loader = ({ isLoading }: LoaderProps) => {
-  if (!isLoading) {
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(isLoading)
+
+  if (!shouldRender) {
     return null
   }
 
   return (
     <ReactPortal containerId="loader-root">
-      <S.Wrapper>
+      <S.Wrapper isLeaving={!isLoading} ref={animatedElementRef}>
         <Spinner size={90} />
       </S.Wrapper>
     </ReactPortal>
--- client/src/components/Loader/styles.ts
+++ client/src/components/Loader/styles.ts
@@ -1,14 +1,35 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+import { ModalStyleProps } from '../Modal/styles'
 
-export const Wrapper = styled.main`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  z-index: 30;
-  top: 0;
-  left: 0;
-  background: rgba(246, 245, 252, 0.7);
+type WrapperProps = Pick<ModalStyleProps, 'isLeaving'>
+
+const fadeIn = keyframes`
+from { opacity: 0; }
+to { opacity: 1; }
+`
+
+const fadeOut = keyframes`
+from { opacity: 1; }
+to { opacity: 0; }
+`
+
+export const Wrapper = styled.main<WrapperProps>`
+  ${({ isLeaving }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    animation: ${fadeIn} 0.3s forwards;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 30;
+    top: 0;
+    left: 0;
+    background: rgba(246, 245, 252, 0.7);
+
+    ${isLeaving &&
+    css`
+      animation: ${fadeOut} 0.3s forwards;
+    `}
+  `}
 `
