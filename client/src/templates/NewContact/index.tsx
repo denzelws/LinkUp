036d4595f3ce3a85fe98ee964ca5a9bf3f2d4ce5
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-13 14:50:16 -0300

    Implement useNewContact hook

--- client/src/templates/NewContact/index.tsx
+++ client/src/templates/NewContact/index.tsx
@@ -1,33 +1,11 @@
-import { useRef } from 'react'
-
 import { Container } from '../../components/Container'
-import { CardProps } from '../../components/Card'
 import PageHeader from '../../components/PageHeader'
 import ContactForm from '../../components/ContactForm'
 
-import { toast } from '../../utils/toast'
-import contactsService from '../../services/ContactsService'
+import { useNewContact } from './useNewContact'
 
 const NewContact = () => {
-  const contactFormRef = useRef<HTMLFormElement>(null)
-
-  const handleSubmit = async (contact: CardProps): Promise<void> => {
-    try {
-      await contactsService.createContact(contact)
-
-      contactFormRef.current?.resetFields()
-
-      toast({
-        type: 'success',
-        text: 'Contato cadastrado com sucesso!'
-      })
-    } catch {
-      toast({
-        type: 'danger',
-        text: 'Ocorreu um erro ao cadastrar o contato!'
-      })
-    }
-  }
+  const { contactFormRef, handleSubmit } = useNewContact()
   return (
     <Container>
       <PageHeader title="Novo Contato" />
