552be66efed6d15d1a20cd36c77a3ee9a42ddaf9
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-13 14:49:36 -0300

    Implement useEditContact hook

--- client/src/templates/EditContact/index.tsx
+++ client/src/templates/EditContact/index.tsx
@@ -1,71 +1,14 @@
-import { useParams, useNavigate } from 'react-router-dom'
-import { useEffect, useRef, useState } from 'react'
-
 import { Container } from '../../components/Container'
-import { CardProps } from '../../components/Card'
 import PageHeader from '../../components/PageHeader'
 import ContactForm from '../../components/ContactForm'
 import Loader from '../../components/Loader'
 
-import contactsService from '../../services/ContactsService'
-import { toast } from '../../utils/toast'
-
-import { useSafeAsyncAction } from '../../hooks/useSafeAsyncAction'
+import useEditContact from '../EditContact/useEditContact'
 
 const EditContact = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [contactName, setContactName] = useState('')
+  const { isLoading, contactName, contactFormRef, handleSubmit } =
+    useEditContact()
 
-  const contactFormRef = useRef<HTMLFormElement>(null)
-
-  const { id } = useParams<{ id: string }>()
-  const navigate = useNavigate()
-
-  const safeAsyncAction = useSafeAsyncAction()
-
-  useEffect(() => {
-    async function loadContacts() {
-      try {
-        const contact: CardProps = await contactsService.getContactById(
-          id ?? ''
-        )
-
-        safeAsyncAction(() => {
-          contactFormRef.current?.SetFieldsValue(contact)
-          setIsLoading(false)
-          setContactName(contact.name)
-        })
-      } catch {
-        safeAsyncAction(() => {
-          navigate('/')
-          toast({
-            type: 'danger',
-            text: 'Contato não encontrado!'
-          })
-        })
-      }
-    }
-
-    loadContacts()
-  }, [id, navigate, safeAsyncAction])
-
-  async function handleSubmit(contact: CardProps): Promise<void> {
-    try {
-      await contactsService.updateContact(id, contact)
-      setContactName(contact.name)
-
-      toast({
-        type: 'success',
-        text: 'Contato editado com sucesso!'
-      })
-    } catch {
-      navigate('/')
-      toast({
-        type: 'danger',
-        text: 'Ocorreu um erro ao editar o contato!'
-      })
-    }
-  }
   return (
     <Container>
       <Loader isLoading={isLoading} />
